b8136c749bb178c4b3c821f02b139b7392232d04
dkk25 <dimoskkm@gmail.com>
2026-07-13 16:05:37 +0300

    add button dark mode

---
+++ script.js
@@ -0,0 +1,11 @@
+const themeBtn = document.getElementById('theme-toggle');
+
+themeBtn.addEventListener('click', function() {
+    document.body.classList.toggle('dark-theme');
+
+    if (document.body.classList.contains('dark-theme')) {
+        themeBtn.textContent = '☀️ Light Mode';
+    } else {
+        themeBtn.textContent = '🌙 Dark Mode';
+    }
+});
